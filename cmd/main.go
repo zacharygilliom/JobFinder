fe3ec9f5c642763c155338fbeac57e9e8d0e349e
Zachary Gilliom <zacharygilliom@gmail.com>
2020-12-10 00:40:24 -0500

    Added Get Messages function to retrieve messages from mailbox.

--- cmd/main.go
+++ cmd/main.go
@@ -1,9 +1,35 @@
 package main
 
 import (
+	"context"
+	"fmt"
+	"log"
+	"time"
+
 	"github.com/zacharygilliom/JobFinder/internal/authtoken"
+	"google.golang.org/api/gmail/v1"
+	"google.golang.org/api/option"
 )
 
 func main() {
 	authtoken.Verify()
+	getMessages()
+}
+
+func getMessages() {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	gmailService, err := gmail.NewService(ctx, option.WithCredentialsFile("../credentials.json"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(gmailService)
+
+	messagesCall := gmailService.Users.Messages.List("me")
+	messagesResp, err := messagesCall.Do()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(messagesResp)
+
 }
